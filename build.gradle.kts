6348df1272e9df2fbaacb02d69af3f46cf29f57b
Eliminater74 <eliminater74@gmail.com>
2024-07-19 19:22:27 -0400

    Initial Port ADBlib from https://android.googlesource.com/platform/tools/base/+/refs/heads/mirror-goog-studio-main/adblib/

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0" apply false
-    id("com.android.application") version "8.0.0" apply false
-    id("com.android.library") version "8.0.0" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("com.android.library") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
 
